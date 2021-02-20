2b0bd01174098949812840d499c04f480e0bd648
Humphrey Kamau <58790731+Grayhkamau@users.noreply.github.com>
2021-02-20 14:47:34 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{
+char fname;
+char password;
+
+printf("enter a first name");
+scanf("%c",password);
+
+if(fname="Humphrey"::password="@hkn2021"){
+printf("welcome %c",fname);
+}
+else{
+    printf("incorrect credentials");
+}
+return 0;
+}
